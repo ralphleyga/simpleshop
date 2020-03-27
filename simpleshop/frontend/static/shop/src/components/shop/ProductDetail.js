784a122be81b2616bf80b856cfe23b01c644eccb
ralph <ralphfleyga@gmail.com>
2020-03-27 16:04:15 +0800

    Add carousel

--- simpleshop/frontend/static/shop/src/components/shop/ProductDetail.js
+++ simpleshop/frontend/static/shop/src/components/shop/ProductDetail.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import ProductCarousel from './ProductCarousel'
+import { connect } from 'react-redux'
 
 
 class ProductDetail extends Component {
+
+    componentWillMount() {
+        console.log(this.props)
+    }
+
     render() {
+        console.log(this.props)
+
         return (
             <section>
+                <ProductCarousel />
                 <section className='jumbotron text-center bg-light'>
-                    <h1>Adidas Super Star</h1>
+                    <h1>Hello</h1>
                     <div className='lear text-muted'>
                     <p>Lorem ipsum dolor sit amet congue lacinia per massa vulputate. Mattis efficitur massa nibh conubia cubilia consequat metus aliquam a. Vehicula gravida torquent scelerisque nam proin. Mattis venenatis dui felis etiam elementum gravida. Dictumst eu magnis potenti placerat senectus cursus urna ligula.</p>
 
@@ -16,10 +25,15 @@ class ProductDetail extends Component {
                     </div>
                 </section>
 
-                <ProductCarousel />
             </section>
         )
     }
 }
 
-export default ProductDetail
+const mapStateToProps = (state) => {
+    return {
+        products: state.productReducer.products,
+    }
+}
+
+export default connect(mapStateToProps)(ProductDetail)
