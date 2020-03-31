a1529bcac27ba281b3dc1022b4a57be59a716716
ralph <ralphfleyga@gmail.com>
2020-03-31 23:04:22 +0800

    optimize axios to use async/await enhancement

--- simpleshop/frontend/static/shop/src/actions/products.js
+++ simpleshop/frontend/static/shop/src/actions/products.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 export const productFetch = () => {
     return async dispatch => {
         let resp = await axios.get('products')
-        let { data } = resp.data;
         return dispatch({type: 'FETCH_PRODUCTS', payload: resp.data })
     }
 }
@@ -11,68 +10,52 @@ export const productFetch = () => {
 export const categoryFetch = () => {
     return async dispatch => {
         let resp =  await axios.get('categories/')
-        let { data } = resp.data
         return dispatch({type: 'FETCH_CATEGORY', payload: resp.data})
     }
 }
 
 export const ordersFetch = () => {
-    return dispatch => {
-        return axios.get('orders/')
-                .then(resp => {
-                    dispatch({type: 'FETCH_ORDERS', payload: resp.data})
-                })
+    return async dispatch => {
+        let resp = await axios.get('orders/')
+        return dispatch({type: 'FETCH_ORDERS', payload: resp.data})
     }
 }
 
 export const addressFetch = () => {
-    return dispatch => {
-        return axios.get('address/')
-                .then(resp => {
-                    dispatch({type: 'FETCH_ADDRESSES', payload: resp.data})
-                })
+    return async dispatch => {
+        let resp = await axios.get('address/')
+        return dispatch({type: 'FETCH_ADDRESSES', payload: resp.data})
     }
 }
 
 export const addCart = item => {
-    return dispatch => {
-        return axios.post('cart/', {
+    return async dispatch => {
+        let resp = await axios.post('cart/', {
                     item: item.id,
                     quantity: 1
                 })
-                .then(resp => {
-                    dispatch({
-                        type: 'CART_ADD_ITEM',
-                        payload: resp.data
-                    })
-                })
+        return dispatch({ type: 'CART_ADD_ITEM', payload: resp.data })
     }
 }
 
 export const updateCart = item => {
-    return dispatch => {
-        return axios.put('cart-update/' + item.id + '/', {
+    return async dispatch => {
+        await axios.put('cart-update/' + item.id + '/', {
                     quantity: item.quantity
                 })
-                .then(resp => {
-                    axios.get('orders/')
-                        .then(resp => {
-                            dispatch({type: 'FETCH_ORDERS', payload: resp.data})
-                        })
-                })
+        let resp = await axios.get('orders/')
+        return  dispatch({type: 'FETCH_ORDERS', payload: resp.data})
     }
 }
 
 export const checkoutCart = item => {
-    return dispatch => {
-        return axios.post('checkout/', {
+    return async dispatch => {
+        await axios.post('checkout/', {
             address: item.address,
             payment_method: 1 // COD
-            }).then(resp => {
-                axios.get('orders/')
-                    .then(resp => {
-                        dispatch({type: 'FETCH_ORDERS', payload: resp.data})
-                    })
             })
+        // pull latest orders data
+        let resp = axios.get('orders/')
+        return dispatch({type: 'FETCH_ORDERS', payload: resp.data})
     }
 }
